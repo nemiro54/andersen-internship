ca3a0690c001c3f5ae33008403ad8d2aa39d5381
Uladzislau Niamira <nemiro54@gmail.com>
2022-12-25 16:09:18 +0300

    feature(andersen-internship) added web.xml & index.jsp

---
+++ src/main/java/Lesson2.java
@@ -0,0 +1,8 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        int a = 3;
+        double b = 2.6;
+        var res = a + b;
+        System.out.println(res);
+    }
+}
